281895e52fb827995b14d25f1f352c382d046d4e
dean06001 <dean06001@E-PW0234KC>
2023-10-19 15:00:19 +0200

    Add project files.

---
+++ inlämnings uppgift 4.4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj ett alternativ");
+            Console.WriteLine("1. Subtrahera ett tal");
+            Console.WriteLine("2. Dividera ett tal");
+            Console.WriteLine("3. Avsluta programmet");
+            string svar = Console.ReadLine();
+
+            switch (svar)
+            {
+                case "1":
+                    Console.WriteLine("Skriv in första talet");
+                    double tal1Minus = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv in andra talet");
+                    double tal2Minus = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{tal1Minus}-{tal2Minus}={tal1Minus - tal2Minus}");
+                    break;
+                case "2":
+                    Console.WriteLine("Skriv in första talet");
+                    double tal1Division = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv in andra talet");
+                    double tal2Division = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{tal1Division}/{tal2Division}={tal1Division / tal2Division}");
+                    break;
+                case "3":
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
